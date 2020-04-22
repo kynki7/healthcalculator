3cd7ee50493c9cc0ed2b6224b0efcaf6fa2dde35
kynki7 <60573059+kynki7@users.noreply.github.com>
2020-04-22 17:21:38 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# healthcalculator
-Health Calculator
+This Python based application is used to calculate several numbers that can be useful in determining your general health.
+
+This application will calculate your Total Daily Energy Expenditure (TDEE), Body Mass Index (BMI), Basic Metabolic Rate (BMR).
+Through these two numbers, you can then select a suitable caloric intake for yourself, depending on your goals.
+Depending on your goals, the calories will be split up into their macronutrients, which will be ideal in achieving your goals.
+
+All information are based on scientific research done online. Majority of research is based on examine.com
+
+
+
+TDEE is a measure of the average amount of calories you burn in a day. In other words, if you consume the number of calories stated in your TDEE, you will maintain your weight.
+Do not that these numbers are only a rough estimate. To get the best use of out these calculators, track your weight regularly and adjust accordingly.
+This calculator has been adjusted to Singaporeans' typical lifestlyes.
+
+BMI is an index used by many health authorities around the world. Although it can be frowned upon by the fitness and lifting community for being inaccurate, it will ultimately be beneficial to keep track of your BMI for the majority of the population.
+
+All units are based on metric.
+
+
+
+
+
+DISCLAIMER: I am not a doctor or a medical professional. 
+Use of this information is strictly at your own risk. 
+Always consult a physician before embarking on a drastic change in nutrition or diet.
+I will not assume any liability for direct or indirect losses or damages that may result from the use of information in the calculator including but not limited to economic loss, injury, illness or death.
---
+++ application.py
@@ -0,0 +1,121 @@
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+    
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    if request.method == "POST":
+        weight = request.form.get("weight")
+        height = request.form.get("height")
+        age = request.form.get("age")
+        gender = request.form.get("gender")
+        activity = request.form.get("activity")
+        goal = request.form.get("goal")
+        if not weight:
+            print("enter weight")
+            return false
+        if not height:
+            print("enter height")
+            return false
+        if not age:
+            print("enter age")
+            return false
+        if not gender:
+            print("enter gender")
+            return false
+        if not activity:
+            print("enter activity")
+            return false
+        if not goal:
+            print("enter goal")
+            return false
+            
+        return redirect(url_for("stats"))
+        
+    else:
+        return render_template("layout.html")
+        
+@app.route("/stats", methods=["GET", "POST"])
+def stats():
+    
+    if request.method == "POST":
+        weight = int(request.form.get("weight"))
+        height = int(request.form.get("height"))
+        age = int(request.form.get("age"))
+        gender = request.form.get("gender")
+        activity = request.form.get("activity")
+        goal = request.form.get("goal")        
+        #Convert height to metres
+        h = height / 100
+        
+        #Calculate BMI to 1 dp
+        bmi = round((weight / (h * h)), 1)
+        
+        #Calculate BMR
+        if gender == "male":
+            bmr = round((66 + (13.7 * weight) + (5 * height) - (6.8 * age)))
+        elif gender == "female":
+            bmr = round((655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)))
+        
+        # Check risk level
+        if bmi >= 18.5 and bmi <= 22.9:
+            risk = "Low Risk"
+        elif bmi >= 23 and bmi <= 27.4:
+            risk = "Moderate Risk"
+        elif bmi >= 27.5:
+            risk = "High Risk"
+        else:
+            risk = "Invalid BMI, input appropriate numbers"
+            
+        #Calculate TDEE
+        if activity == "low":
+            tdee = round(1.2 * bmr)
+        elif activity == "medium":
+            tdee = round(1.53 * bmr)
+        elif activity == "high":
+            tdee = round(1.88 * bmr)
+        
+        #Sort out macros
+        if goal == "lw":
+            total_calories = round(tdee * 0.85)
+            protein = round(2.3 * weight)
+            remaining_calories = total_calories - (protein * 4)
+            carbs = round(((0.63 * remaining_calories) / 4))
+            fat = round(((0.37 * remaining_calories) / 9))
+        elif goal == "gm":
+            total_calories = round(tdee * 1.1)
+            protein = round(1.6 * weight)
+            remaining_calories = total_calories - (protein * 4)
+            carbs = round(((0.63 * remaining_calories) / 4))
+            fat = round(((0.37 * remaining_calories) / 9))
+        elif goal == "mm":
+            total_calories = round(tdee)
+            protein = round(0.8 * weight)
+            remaining_calories = total_calories - (protein * 4)
+            carbs = round(((0.63 * remaining_calories) / 4))
+            fat = round(((0.37 * remaining_calories) / 9))
+            
+        return render_template("stats.html", bmi=bmi, bmr=bmr, tdee=tdee, 
+                                total_calories=total_calories, risk=risk, 
+                                protein=protein, carbs=carbs, fat=fat)
+                                
+    else:
+        return render_template("layout.html")
+    
